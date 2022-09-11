90e7bb2b09b989ee4384af032329e9b27e498684
jkatharine <julia.katharineteixeirasilva-ext@ab-inbev.com>
2022-09-11 00:39:08 -0300

    axios request mapping

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,11 +1,12 @@
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 import NotificationButton from "../NotificationButton";
 import './style.css';
+import axios from "axios";
 
 function SalesCard() {
 
@@ -15,6 +16,12 @@ function SalesCard() {
 
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
+
+    useEffect( () => {
+        axios.get("http://localhost:8080/sales").then(response => {
+            console.log(response.data)
+        })
+    }, []);
 
     return (
         <>
