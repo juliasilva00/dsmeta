c6ea7e608365eea252b657bc0b98bc0bc8c2536b
jkatharine <julia.katharineteixeirasilva-ext@ab-inbev.com>
2022-09-11 01:05:00 -0300

    filter sales by date

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -22,10 +22,16 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect( () => {
-        axios.get(`${BASE_URL}/sales`).then(response => {
+
+        const dmin = minDate.toISOString().slice(0,10);
+        const dmax = maxDate.toISOString().slice(0,10);
+
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(response => {
             setSales(response.data.content)
         })
-    }, []);
+    }, [minDate,maxDate]);
+    //quando colocamos algun valor na chave da linha 34 falamos para o useEffect rodas novamente quando esses valores mudarem
 
     return (
         <>
